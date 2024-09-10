07a99d1790c4b476adf4553774b019a6ad01e8d7
OscarPineda1 <167844008+OscarPineda1@users.noreply.github.com>
2024-09-09 21:08:20 -0500

    ramaviaje

--- obj/Debug/net8.0/Mi mascota.AssemblyInfo.cs
+++ obj/Debug/net8.0/Mi mascota.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mi mascota")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e2fd9c8d014b16bec05088122d4e9cd3d60ec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85575bc5bedca98e21ac9f4892487ebd9bf66e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mi mascota")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mi mascota")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
